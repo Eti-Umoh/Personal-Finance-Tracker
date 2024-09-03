4692d18af8cb670538265143bef1252ba4135c03
Eti-Umoh <etiumoh04@gmail.com>
2024-09-03 09:56:32 +0100

    db credentials change

--- db.js
+++ db.js
@@ -1,16 +1,22 @@
 import { Sequelize } from "sequelize";
-const db_name = process.env.DB_NAME
-const db_host = process.env.DB_HOST
-const db_password = process.env.DB_PASSWORD
-const db_user = process.env.DB_USER
-const db_dialect = process.env.DB_DIALECT
-const db_port = parseInt(process.env.DB_PORT);
+const dbName = process.env.DB_NAME;
+const dbHost = process.env.DB_HOST;
+const dbPassword = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+const dbDialect = process.env.DB_DIALECT;
+const dbPort = parseInt(process.env.DB_PORT, 10);
 
 
-const sequelize = new Sequelize(db_name, db_user, db_password, {
-    host: db_host,
-    dialect: db_dialect,
-    port: db_port,
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: dbDialect,
+    port: dbPort,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,  // You can set this to true if you want to strictly validate the server's SSL certificate.
+        },
+    },
 });
 
 export default sequelize;
