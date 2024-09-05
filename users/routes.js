3c2ef56e74f586fc4f685aba646a265d4135bbb3
Eti-Umoh <etiumoh04@gmail.com>
2024-09-05 15:02:35 +0100

    router

--- users/routes.js
+++ users/routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const userRouter = express.Router();
-import { createUser } from './utils.js';
+import { createUser, getCurrentUser } from './utils.js';
 
 
 userRouter.post('/signup', createUser);
+userRouter.get('/', getCurrentUser);
 
 export default userRouter;
