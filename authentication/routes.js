630b141197dd2cc1bad79f7eca0a7ecc72242579
Eti-Umoh <etiumoh04@gmail.com>
2024-09-06 16:12:01 +0100

    set up routes

--- authentication/routes.js
+++ authentication/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const authRouter = express.Router();
-import { login, logOut, changePassword } from './utils.js';
+import { login, logOut, changePassword,
+    resetPassword, sendResetToken } from './utils.js';
 
 
 authRouter.post('/login', login);
 authRouter.post('/logout', logOut);
 authRouter.patch('/password', changePassword);
+authRouter.put('/password', resetPassword);
+authRouter.post('/sendToken', sendResetToken);
 
 export default authRouter;
